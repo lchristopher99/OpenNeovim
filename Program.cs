572ad930a5c3464d98550ce889f85b824ee93c7a
logan <logan.christopher@comcast.net>
2023-11-03 00:54:47 -0500

    - Initial Commit

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System.Diagnostics;
+
+public class Program 
+{
+    public static void Main(string[] args)
+    {
+        if (args.Length == 0) return;
+
+        var p = new ProcessStartInfo()
+        {
+            UseShellExecute = true,
+            CreateNoWindow = true,
+            FileName = "pwsh",
+            Arguments = $"-noexit -c nv {args[0]}"
+        };
+
+        try { Process.Start(p); }
+        catch {}
+    }
+}
